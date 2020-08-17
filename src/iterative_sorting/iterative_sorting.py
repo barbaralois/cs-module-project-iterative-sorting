1ab447017e831593aa20ba35abca9f2db8ae6d16
Barbara Moore <barbaraloismoore@gmail.com>
2020-08-17 17:26:32 -0400

    mvp completed (iterative sorting and searching files)

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,24 +2,30 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
+        # set a variable for the smallest index, that gets larger as we sort more of the array
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
-        # Your code here
+        if smallest_index != i:
+            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
-
+    sorted_i = len(arr)-1
+    while sorted_i > 0:
+        for i in range(0, sorted_i):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+            continue
+        sorted_i -= 1
     return arr
 
 '''
